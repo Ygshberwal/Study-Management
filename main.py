f3907d6fd375b884a0f1f0515a5f63a34c35d174
Ygshberwal26 <yogeshberwal266@gmail.com>
2024-04-04 15:07:03 +0530

    Backend: L1 & L2

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_security import SQLAlchemyUserDatastore, Security
+from application.models import db, User, Role
+from config import DevelopmentConfig
+from application.resources import api
+
+
+def create_app():
+    app=Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+    api.init_app(app)
+    datastore=SQLAlchemyUserDatastore(db, User, Role)
+    app.security=Security(app, datastore)
+    with app.app_context():
+        import application.views
+
+    return app, datastore
+app, datastore =create_app()
+
+if __name__=='__main__':
+    app.run(debug=True)
+
---
+++ upload_initial_data.py
@@ -0,0 +1,17 @@
+from main import app, datastore
+from application.models import db, Role
+from flask_security import hash_password 
+
+with app.app_context():
+    db.create_all()
+    datastore. find_or_create_role(name="admin",description="User is an admin")
+    datastore. find_or_create_role(name="inst", description="User is an Instructor")
+    datastore. find_or_create_role(name="stud", description="User is a Student") 
+    db.session.commit()
+    if not datastore.find_user(email="admin@email.com"): 
+        datastore.create_user(email="admin@email.com",password=hash_password("admin"), roles=["admin"])
+    if not datastore.find_user(email="inst1@email.com"): 
+        datastore.create_user(email="inst1@email.com", password=hash_password("inst1"), roles=["inst"],active=False)
+    if not datastore.find_user(email="stud1@email.com"):
+        datastore.create_user(email="stud1@email.com",password=hash_password("stud1"), roles=["stud"])
+    db.session.commit()
